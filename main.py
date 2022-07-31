f9d04443d696e02a7ddac84d697ad3aca2f4c45f
Sindromo <lis.mateusz@ymail.com>
2022-07-31 15:33:44 +0200

    init

---
+++ main.py
@@ -0,0 +1,79 @@
+import time
+import pyautogui
+from pynput.mouse import Listener
+from ctypes import *
+
+# Click on the start light in minigame to get its position on screen and color
+# Set False to start racing
+get_start_light_screen_location = False
+get_color_from = (windll.user32.GetDC(0), 839, 503)
+gray = 12961221
+red = 7294194
+
+
+# get screen loc for start lights
+def on_click(x, y, button, pressed):
+    pos_x = []
+    pos_y = []
+    if pressed:
+        pos_x.append(x)
+        pos_y.append(y)
+        color = windll.gdi32.GetPixel(windll.user32.GetDC(0), pos_x[0], pos_y[0])
+        if len(pos_x) == 1:
+            listener.stop()
+            print(pos_x[0], pos_y[0], color)
+
+
+def space(seconds):
+    pyautogui.keyDown('space')
+    time.sleep(seconds)
+    pyautogui.keyUp('space')
+
+
+def space_boost(seconds):
+    pyautogui.keyDown('space')
+    pyautogui.press('shift')
+    time.sleep(seconds)
+    pyautogui.keyUp('space')
+
+
+def wait(seconds):
+    time.sleep(seconds)
+
+
+def race_start():
+    space(1.767)  # z1
+    wait(0.33)
+    space(4.824)  # z2
+    wait(0.39)
+    space(4.36)  # z3
+    wait(0.31)
+    space(3.88)  # z4 bull
+    wait(0.26)
+    space(1.56)  # z5
+    # wait(0.0001)
+    space(2.09)  # z6
+    wait(0.06)
+    space(1)  # z7
+    wait(0.01)
+    space_boost(5)
+    quit()
+
+
+def main():
+    while True:
+        color = windll.gdi32.GetPixel(*get_color_from)
+        if color == red:
+            y = True
+            while y:
+                color = windll.gdi32.GetPixel(*get_color_from)
+                if color == gray:
+                    race_start()
+
+
+if __name__ == "__main__":
+    if get_start_light_screen_location:
+        with Listener(on_click=on_click) as listener:
+            listener.join()
+        quit()
+    main()
